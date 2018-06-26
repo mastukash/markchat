fe02226abd31cfd497eab4fdeffa4b16a077f325
mastukash <mastykash@itstep.org>
2018-06-26 15:58:35 +0300

    коміт перед злиттям проекту

--- markchat/MarkChat.DAL/Entities/TagChat.cs
+++ markchat/MarkChat.DAL/Entities/TagChat.cs
@@ -13,6 +13,7 @@ namespace MarkChat.DAL
         public int Id { get; set; }
         public string Name { get; set; }
         public string InvitationCode { get; set; }
+        public string Description { get; set; }
 
         public TagChat()
         {
